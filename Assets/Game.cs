73319ce016ba6662d8e7ab7d8fbdb8ef396035ad
Daniel Hix <danieljacobhix@gmail.com>
2020-04-17 14:40:37 -0500

    Initial push of the project

---
+++ Assets/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameState state;
+
+    private void Awake()
+    {
+        state = GameState.Play;
+    }
+}
+
+public enum GameState
+{
+    Play,
+    Pause
+}
