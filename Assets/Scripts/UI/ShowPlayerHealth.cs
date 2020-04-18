e966af903837ed41e8154753239711e09c0ad17e
Tarkonner <41646766+Tarkonner@users.noreply.github.com>
2020-04-18 20:40:29 +0200

    Player UI

---
+++ Assets/Scripts/UI/ShowPlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPlayerHealth : MonoBehaviour
+{
+    [SerializeField] private GameObject UIHeart;
+
+    [SerializeField] private float wide = 25;
+    
+    
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 pos = new Vector3(wide * i + wide / 2, -wide / 2, 0) + transform.position;
+            var spawn = Instantiate(UIHeart, pos, Quaternion.identity);
+            spawn.transform.SetParent(transform);
+        }
+    }
+}
